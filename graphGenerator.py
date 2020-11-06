17a9ec09e72fa577835af4d01a304fa7ee6f0286
Daniel <danieldelacruzprieto@gmail.com>
2020-11-06 00:28:46 -0500

    some changes

--- graphGenerator.py
+++ graphGenerator.py
@@ -1,4 +1,5 @@
 
+from os import close
 import graph
 import random 
 
@@ -30,6 +31,58 @@ def generate (n , m ):
     return g 
 
 
-g = generate(5,3) 
+#g = generate(5,3) 
 
-g.printGraph()
+#g.printGraph()
+
+import os 
+
+def generateFolderofGraph (name , ngraph):
+    
+    path = os.path.join(os.getcwd(), name)
+    if os.path.exists(path):
+        print (path + ' already exist')
+    else :
+        os.makedirs(path)
+
+    file , ext  = "graph" , "txt"
+
+    i = 1 
+    while (i <= ngraph ):
+        path = os.path.join (path , file + str (i) + '.'  + ext)
+        print (path) 
+        baconFile = open (path , 'w')
+        baconFile.close() 
+        path = os.path.dirname (path) 
+        i = i +1 
+
+    
+    
+
+foldername = "graph"
+generateFolderofGraph (foldername , 100 ) 
+
+
+def clearFolder (folder):
+    for folderName ,subFolders , fileNames  in os.walk(os.getcwd()):
+        if os.path.basename (folderName ) == folder :
+            #print ('The current Folder is : '  + folderName ) 
+
+            for fileName in fileNames :
+                if fileName.endswith('.txt'):
+                    fullpath = os.path.join(folderName , fileName)
+                    print('deleted :' +  fullpath)
+                    os.unlink(fullpath)
+                    
+            #print ("ES AQUI _______________________________________________________")
+
+        #for subFolder in subFolders : 
+        #    print ('SUBFOLDER OF : ' + folderName + ' : ' + subFolder)
+
+        #for fileName in fileNames: 
+        #    print ('FILE INSIDE ' + folderName + ': ' + fileName)
+        
+        #print (' ') 
+
+clearFolder ('graph') 
+       
